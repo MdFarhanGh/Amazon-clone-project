ce79641b26d5084f6436c9c4a9059fcfb6bea605
MdFarhanGh <mohammedfarhan197197@gmail.com>
2025-12-23 16:28:55 +0800

    Delete products from cart when clicking delete.

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
 }, {
@@ -30,4 +30,19 @@ export function addToCart(productId) {
       quantity: quantityValue
     });
   } 
+}
+
+
+/*Steps to remove the item from the cart when we click delete.  
+1. Create a new array.
+2. Loop through the cart
+3. Add each product to the new array, except for this productId. */
+export function removeFromCart(productId) {
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart
 }
