935c83b090ce4e1d666f1ec7531d83d63613ffa5
Farhan <mohammedfarhan197197@gmail.com>
2026-01-12 18:35:52 +0800

    Learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -107,3 +107,15 @@ export function ifInvalidInput(productId) {
     invalidInput.innerHTML = '';
   }, 2500)
 }
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+    fun();
+  });
+  
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
